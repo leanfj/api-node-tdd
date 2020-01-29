d5fd0b9ad883a4d682ea1f69e3a908e58f9a52a8
leanfj <leanfj@hotmail.com>
2020-01-29 19:51:26 -0300

    Inciando teste unitarios

---
+++ src/controllers/clients.js
@@ -0,0 +1,3 @@
+class ClientsController {}
+
+export default ClientsController;
---
+++ test/unit/controllers/clients_spec.js
@@ -0,0 +1,9 @@
+import ClientsController from "../../../src/controllers/clients";
+
+describe("Controllers: Clients", () => {
+    describe("get() clients", () => {
+        it("should return a list of clients", () => {
+            const clientsController = new ClientsController();
+        });
+    });
+});
