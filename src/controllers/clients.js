132b86e2d5f8e088249c6de6d334be798d924717
leanfj <leanfj@hotmail.com>
2020-01-30 18:41:04 -0300

    Implementação de test para controller de clients

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -1,3 +1,14 @@
-class ClientsController {}
+class ClientsController {
+    get(req, res) {
+        return res.send([
+            {
+                name: "Default Client Name",
+                origin: "Elo7",
+                birthDate: "22/03/1986",
+                cellPhone: "(21)96883-6199"
+            }
+        ]);
+    }
+}
 
 export default ClientsController;
--- test/unit/controllers/clients_spec.js
+++ test/unit/controllers/clients_spec.js
@@ -1,9 +1,29 @@
 import ClientsController from "../../../src/controllers/clients";
 
+import sinon from "sinon";
+
 describe("Controllers: Clients", () => {
+    const defaultClient = [
+        {
+            name: "Default Client Name",
+            origin: "Elo7",
+            birthDate: "22/03/1986",
+            cellPhone: "(21)96883-6199"
+        }
+    ];
+
     describe("get() clients", () => {
         it("should return a list of clients", () => {
+            const request = {};
+            const response = {
+                send: sinon.spy()
+            };
+
             const clientsController = new ClientsController();
+            clientsController.get(request, response);
+
+            expect(response.send.called).to.be.true;
+            expect(response.send.calledWith(defaultClient)).to.be.true;
         });
     });
 });
